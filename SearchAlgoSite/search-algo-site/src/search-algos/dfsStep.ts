4edd919ddb17ca6cfa94d6edb835def6dbb0e303
bryman360 <bryman360@yahoo.com>
2025-01-03 15:53:12 -0800

    Forgot to commit dfsStep

---
+++ SearchAlgoSite/search-algo-site/src/search-algos/dfsStep.ts
@@ -0,0 +1,36 @@
+import { moves } from "@/lib/common";
+import { GridCell } from "../../types";
+
+export function dfsStep(gridState: Array<Array<GridCell>>, stack: Array<Array<number>>, goalLoc: Array<number>) {
+    const currentSquare = stack.pop();
+    
+    if (!currentSquare) return;
+
+    if (gridState[currentSquare[0]][currentSquare[1]].state == 'goal') {
+        return true;
+    }
+
+    if (!currentSquare) {
+        throw Error('Trying to move through a DFS with no stack.');
+    }
+    for(const move of moves) {
+        const nextMove = [currentSquare[0] + move[0], currentSquare[1] + move[1]];
+        console.log("Next Move:", nextMove)
+        if (nextMove[0] >= 0 &&
+            nextMove[0] < gridState.length &&
+            nextMove[1] >= 0 &&
+            nextMove[1] < gridState[0].length &&
+            (gridState[nextMove[0]][nextMove[1]].state == null || gridState[nextMove[0]][nextMove[1]].state == 'goal' || gridState[nextMove[0]][nextMove[1]].state == 'frontier')) {
+                gridState[nextMove[0]][nextMove[1]].parentRow = currentSquare[0];
+                gridState[nextMove[0]][nextMove[1]].parentCol = currentSquare[1];
+                if (gridState[nextMove[0]][nextMove[1]].state == null) gridState[nextMove[0]][nextMove[1]].state = 'frontier';
+                stack.push(nextMove);
+                console.log("Adding", nextMove, "to the stack");
+            }
+    }
+
+    if (gridState[currentSquare[0]][currentSquare[1]].state != 'start') gridState[currentSquare[0]][currentSquare[1]].state = 'explored';
+
+    return false;
+
+}
