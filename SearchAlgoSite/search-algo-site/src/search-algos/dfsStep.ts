9c1da8c4544fafdf43c4f16e47fe584b23129d5f
bryman360 <bryman360@yahoo.com>
2025-01-05 00:31:08 -0800

    DFS and BFS account for mud squares

--- SearchAlgoSite/search-algo-site/src/search-algos/dfsStep.ts
+++ SearchAlgoSite/search-algo-site/src/search-algos/dfsStep.ts
@@ -4,30 +4,29 @@ import { GridCell } from "../../types";
 export function dfsStep(gridState: Array<Array<GridCell>>, stack: Array<Array<number>>, goalLoc: Array<number>) {
     const currentSquare = stack.pop();
     
-    if (!currentSquare) return;
+    if (currentSquare == null) return;
 
-    if (gridState[currentSquare[0]][currentSquare[1]].state == 'goal') {
-        return true;
-    }
+    if (gridState[currentSquare[0]][currentSquare[1]].state == 'goal') return true;
 
-    if (!currentSquare) {
-        throw Error('Trying to move through a DFS with no stack.');
-    }
+    if (!currentSquare) throw Error('Trying to move through a DFS with no stack.');
+    
     for(const move of moves) {
         const nextMove = [currentSquare[0] + move[0], currentSquare[1] + move[1]];
         if (nextMove[0] >= 0 &&
             nextMove[0] < gridState.length &&
             nextMove[1] >= 0 &&
             nextMove[1] < gridState[0].length &&
-            (gridState[nextMove[0]][nextMove[1]].state == null || gridState[nextMove[0]][nextMove[1]].state == 'goal' || gridState[nextMove[0]][nextMove[1]].state == 'frontier')) {
+            (gridState[nextMove[0]][nextMove[1]].state != 'wall' && gridState[nextMove[0]][nextMove[1]].state != 'start' && gridState[nextMove[0]][nextMove[1]].state != 'explored' && gridState[nextMove[0]][nextMove[1]].state != 'mudExplored')) {
                 gridState[nextMove[0]][nextMove[1]].parentRow = currentSquare[0];
                 gridState[nextMove[0]][nextMove[1]].parentCol = currentSquare[1];
-                if (gridState[nextMove[0]][nextMove[1]].state == null) gridState[nextMove[0]][nextMove[1]].state = 'frontier';
+                if (gridState[nextMove[0]][nextMove[1]].state == 'mud') gridState[nextMove[0]][nextMove[1]].state = 'mudFrontier';
+                else if (gridState[nextMove[0]][nextMove[1]].state == null) gridState[nextMove[0]][nextMove[1]].state = 'frontier';
                 stack.push(nextMove);
             }
     }
 
-    if (gridState[currentSquare[0]][currentSquare[1]].state != 'start') gridState[currentSquare[0]][currentSquare[1]].state = 'explored';
+    if (gridState[currentSquare[0]][currentSquare[1]].state == 'mudFrontier') gridState[currentSquare[0]][currentSquare[1]].state = 'mudExplored';
+    else if (gridState[currentSquare[0]][currentSquare[1]].state != 'start') gridState[currentSquare[0]][currentSquare[1]].state = 'explored';
 
     return false;
 
